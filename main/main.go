4dba0a36049ce1dd03110d8e257f1d9f59595f64
Vijay <vshukla684@gmail.com>
2019-05-27 11:39:57 +0530

    Basic Server

--- main/main.go
+++ main/main.go
@@ -1,5 +1,21 @@
 package main
 
+/*
+-http.ListenAndServe
+--Listens for, and responds to, http requests
+--Handles each request using go routines
+---Lightweight concurrency
+---This is multiplexing
+-http.Handle
+--Handle a URL request
+--Maps a URL to any Type (“object”) implementing the handler interface
+-http.HandleFunc
+--Handles a URL request
+--Maps a URL to a func
+---“Wrapper” around a function
+---Turns any function to a handler
+
+ */
 import (
 	"log"
 	"net/http"
