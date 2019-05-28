14dfd1c0bcfcafe0e698b49de8044e6de624986f
Vijay <vshukla684@gmail.com>
2019-05-28 12:57:42 +0530

    Make it more clear

--- main.go
+++ main.go
@@ -28,27 +28,35 @@ First Template
 </html
 `
 
-func Home(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "text/html")
-	tmpl, err := template.New("name").Parse(doc)
-	if err == nil {
-		context := Context{"todd", "more go, please"}
-		tmpl.Execute(writer, context)
-	}
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", Home)
-	r.HandleFunc("/contact", Contact)
-	r.NotFoundHandler = http.HandlerFunc(NotFound)
+	r.HandleFunc("/", ToddFunc)
+	r.HandleFunc("/contact", ContactFunc)
+	r.NotFoundHandler = http.HandlerFunc(NotFoundFunc)
+	//Static File Serve
+	/*r.
+	PathPrefix("/static/").
+	Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	*/
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func Contact(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, "<h1>Contact Us</h1>")
-}
-func NotFound(writer http.ResponseWriter, request *http.Request) {
+func NotFoundFunc(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
 	writer.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(writer, "<h1>Page Not Exist</h1>")
+	fmt.Fprint(writer, "<h1>We Couldn't Found Page</h1>")
+}
+func ContactFunc(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(writer, "<div>To get in touch, please send an email to "+
+		"<a href=\"mailto:vijay@nexthoughts.com\">vijay@nexthoughts.com</a></div>")
+}
+
+func ToddFunc(writer http.ResponseWriter, request *http.Request) {
+	tmpl, err := template.New("name").Parse(doc)
+
+	if err == nil {
+		context := Context{"todd", "more go, please"}
+		log.Fatal(tmpl.Execute(writer, context))
+	}
 }
